6c235949513124fdbefcfc05d55666af7531dc53
ExcellentNonsense <ExcellentNonsense.feedback@gmail.com>
2018-12-08 14:36:35 +0300

    Apply the concept to the VS project.

---
+++ Nonsense.Tests/ActiveRouteTagHelperTests.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+using Nonsense.Infrastructure.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nonsense.Tests {
+    public class ActiveRouteTagHelperTests {
+        [Fact]
+        public void ProcessTag_ActiveLink_AddClass() {
+            // Arrange
+            var context = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                "myuniqueid");
+
+            var output = new TagHelperOutput("a",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult<TagHelperContent> (new DefaultTagHelperContent()));
+
+            var viewContext = new ViewContext();
+            viewContext.RouteData = new RouteData();
+            viewContext.RouteData.Values.Add("Controller", "Home");
+            viewContext.RouteData.Values.Add("Action", "Index");
+
+            // Act
+            var tagHelper = new ActiveRouteTagHelper {
+                Controller = "Home",
+                Action = "Index",
+                ViewContextData = viewContext
+            };
+            tagHelper.Process(context, output);
+
+            // Assert
+            Assert.Equal("active_link", output.Attributes["class"].Value);
+        }
+
+        [Fact]
+        public void ProcessTag_InactiveLink_NotAddClass() {
+            // Arrange
+            var context = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                "myuniqueid");
+
+            var output = new TagHelperOutput("a",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+
+            var viewContext = new ViewContext();
+            viewContext.RouteData = new RouteData();
+            viewContext.RouteData.Values.Add("Controller", "Home");
+            viewContext.RouteData.Values.Add("Action", "Index");
+
+            // Act
+            var tagHelper = new ActiveRouteTagHelper {
+                Controller = "Profile",
+                Action = "Index",
+                ViewContextData = viewContext
+            };
+            tagHelper.Process(context, output);
+
+            // Assert
+            Assert.Null(output.Attributes["class"]);
+        }
+    }
+}
---
+++ Nonsense/Infrastructure/TagHelpers/ActiveRouteTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace Nonsense.Infrastructure.TagHelpers {
+    [HtmlTargetElement("a", Attributes = "is-active-route")]
+    public class ActiveRouteTagHelper : TagHelper {
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContextData { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output) {
+            string currentController = ViewContextData.RouteData.Values["Controller"].ToString();
+            string currentAction = ViewContextData.RouteData.Values["Action"].ToString();
+
+            if (!string.IsNullOrWhiteSpace(Controller) &&
+                !string.IsNullOrWhiteSpace(Action) &&
+                Controller.Equals(currentController, StringComparison.CurrentCultureIgnoreCase) &&
+                Action.Equals(currentAction, StringComparison.CurrentCultureIgnoreCase)) {
+                output.Attributes.SetAttribute("class", "active_link");
+            }
+
+            output.Attributes.RemoveAll("is-active-route");
+        }
+    }
+}

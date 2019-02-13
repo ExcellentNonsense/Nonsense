59ec630ce9f4cc56c5ea0ad41176ba8d09b973b5
ExcellentNonsense <ExcellentNonsense.feedback@gmail.com>
2019-02-13 22:00:15 +0300

    Improve the admin part UI.

--- Nonsense.MvcApp/gulpfile.js
+++ Nonsense.MvcApp/gulpfile.js
@@ -7,22 +7,27 @@ const paths = {
   webroot: "./wwwroot/"
 };
 
-paths.css = paths.webroot + "css/**/*.css";
-paths.processedCssDest = paths.webroot + "css/site.css";
+paths.mainCss = paths.webroot + "css/main/**/*.css";
+paths.processedMainCss = paths.webroot + "css/site.css";
+
+paths.areasCss = paths.webroot + "css/areas/*.css";
+paths.processedAreasCss = paths.webroot + "css/";
+paths.processedAdminCss = paths.webroot + "css/admin.css";
 
 paths.js = paths.webroot + "js/**/*.js";
-paths.processedJsDest = paths.webroot + "js/site.js";
+paths.processedJs = paths.webroot + "js/site.js";
 
 gulp.task("clean", () => {
   const del = require("del");
 
   return del([
-    paths.processedCssDest,
-    paths.processedJsDest
+    paths.processedMainCss,
+    paths.processedAdminCss,
+    paths.processedJs
   ]);
 });
 
-gulp.task("process:css", () => {
+gulp.task("process:mainCss", () => {
   const postcss = require("gulp-postcss");
   const autoprefixer = require("autoprefixer");
   const cssnano = require("cssnano");
@@ -32,19 +37,34 @@ gulp.task("process:css", () => {
     cssnano()
   ];
 
-  return gulp.src([paths.css, "!" + paths.processedCssDest])
-    .pipe(concat(paths.processedCssDest))
+  return gulp.src(paths.mainCss)
+    .pipe(concat(paths.processedMainCss))
     .pipe(postcss(processors))
     .pipe(gulp.dest("."));
+});
+
+gulp.task("process:areasCss", () => {
+  const postcss = require("gulp-postcss");
+  const autoprefixer = require("autoprefixer");
+  const cssnano = require("cssnano");
+
+  const processors = [
+    autoprefixer(),
+    cssnano()
+  ];
+
+  return gulp.src(paths.areasCss)
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(paths.processedAreasCss));
 });
 
 gulp.task("process:js", () => {
   const terser = require('gulp-terser');
 
-  return gulp.src([paths.js, "!" + paths.processedJsDest])
-    .pipe(concat(paths.processedJsDest))
+  return gulp.src([paths.js, "!" + paths.processedJs])
+    .pipe(concat(paths.processedJs))
     .pipe(terser())
     .pipe(gulp.dest("."));
 });
 
-gulp.task("build", gulp.series(["clean", "process:css", "process:js"]));
+gulp.task("build", gulp.series(["clean", "process:mainCss", "process:areasCss", "process:js"]));

b715a5420ee912d709faabfbcbed2a540c95c14b
ExcellentNonsense <ExcellentNonsense.feedback@gmail.com>
2019-02-13 22:05:52 +0300

    Add the application users seeding.

---
+++ Nonsense.Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nonsense.Infrastructure.Identity {
+
+    public class AppIdentityDbContextSeed {
+
+        public static async Task Seed(UserManager<ApplicationUser> userManager) {
+            if (!userManager.Users.Any()) {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                string resourceName = "Nonsense.Infrastructure.SeedData.ApplicationUsers.csv";
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName)) {
+                    using (StreamReader reader = new StreamReader(stream, Encoding.UTF8)) {
+                        string line;
+                        string password = "Secret_pass#1";
+
+                        while ((line = reader.ReadLine()) != null) {
+                            string[] values = line.Split(',');
+
+                            var user = new ApplicationUser {
+                                UserName = values[0],
+                                Email = values[1]
+                            };
+
+                            var result = await userManager.CreateAsync(user, password);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Nonsense.MvcApp/Program.cs
+++ Nonsense.MvcApp/Program.cs
@@ -1,13 +1,23 @@
 ﻿using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Nonsense.Infrastructure.Identity;
 
 namespace Nonsense.MvcApp {
 
     public class Program {
 
         public static void Main(string[] args) {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope()) {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                AppIdentityDbContextSeed.Seed(userManager).Wait();
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
